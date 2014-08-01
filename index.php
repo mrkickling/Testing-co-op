3f3b3f42b422927179460e5c5e0910361c48e0a3
mrkickling <loxdalen@gmail.com>
2014-08-01 12:32:33 +0200

    starting outq
    nive

---
+++ index.php
@@ -0,0 +1,20 @@
+	<!-- Copyright Joakim Loxdal!-->
+	<!DOCTYPE html>
+	<html>
+	<meta charset='utf-8'>
+		<head>
+			<title>Joakim Loxdal</title>
+		</head>
+		<body>
+			<header>
+				<ul>
+					<li><a href="index.php">Start</a></li>
+					<li><a href="omoss.php">Om oss</a></li>
+					<li><a href="blimedlem.php">Bli medlem</a></li>
+					<li><a href="#">Event</a></li>
+					<li><a href="#">Kontakt</a></li>
+					<li><a href="#">Styrelsen</a></li>
+				</ul>
+			</header>
+		</body>
+	</html>
